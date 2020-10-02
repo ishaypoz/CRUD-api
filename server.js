5a8d5fa7bb69fbc6244972fa44b335c5fe98950a
Ishay Pozmantir <59173189+ishaypoz@users.noreply.github.com>
2020-10-02 18:46:38 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 
 //Inventory Object
 let items = [];
+let id = 1;
 
 //GET Items List
 app.get('/items', function (req, res) {
@@ -18,10 +19,22 @@ app.get('/items', function (req, res) {
 
 //GET Item
 app.get('/items/:id', function (req, res) {
-	if (items[req.params.id]) {
-		return res.status(200).json(items[req.params.id]);
+	let index = items.findIndex((x) => x.id == req.params.id);
+	if (index !== -1) {
+		return res.status(200).json(items[index]);
 	} else {
 		res.status(404).json({});
+	}
+});
+
+//Delete Item
+app.delete('/items/:id', function (req, res) {
+	let index = items.findIndex((x) => x.id == req.params.id);
+	if (index !== -1) {
+		items.splice(index, 1);
+		return res.status(200).json('Deleted');
+	} else {
+		return res.status(404).json('Unfound');
 	}
 });
 
@@ -36,12 +49,13 @@ app.post(
 			return res.status(406).json('Not Acceptable');
 		} else {
 			let data = {
-				id: items.length,
+				id: id,
 				name: req.body.name,
 				description: req.body.description,
 				count: req.body.count
 			};
 			items.push(data);
+			id++;
 			return res.status(201).json('Created');
 		}
 	}
@@ -53,7 +67,8 @@ app.put(
 	[check('name').isLength({ min: 3 }), check('description').notEmpty(), check('count').isInt({ min: 0 })],
 	function (req, res) {
 		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
+		let index = items.findIndex((x) => x.id == req.params.id);
+		if (!errors.isEmpty() || index === -1) {
 			console.log(errors);
 			return res.status(406).json('Not Acceptable');
 		} else {
@@ -63,17 +78,40 @@ app.put(
 				description: req.body.description,
 				count: req.body.count
 			};
-			items[req.params.id] = data;
+			items[index] = data;
 			return res.status(200).json('Updated');
 		}
 	}
 );
 
-//Delete Item
-app.delete('/items/:id', function (req, res) {
-	let index = items.findIndex((x) => x.id === req.params.id);
-	items.splice(index, 1);
-	return res.status(200).json('Deleted');
+//PUT withdraw item count
+app.put('/items/:id/withdraw', [check('withdraw').isInt({ min: 0 })], function (req, res) {
+	const errors = validationResult(req);
+	let index = items.findIndex((x) => x.id == req.params.id);
+	if (!errors.isEmpty() || index === -1) {
+		console.log(errors);
+		return res.status(406).json('Not Acceptable');
+	} else {
+		if (items[index].count - req.body.withdraw < 0) {
+			return res.status(406).json('Out of Balance');
+		} else {
+			items[index] = { ...items[index], count: items[index].count - req.body.withdraw };
+			return res.status(200).json('Updated');
+		}
+	}
+});
+
+//PUT deposit item count
+app.put('/items/:id/deposit', [check('deposit').isInt({ min: 0 })], function (req, res) {
+	const errors = validationResult(req);
+	let index = items.findIndex((x) => x.id == req.params.id);
+	if (!errors.isEmpty() || index === -1) {
+		console.log(errors);
+		return res.status(406).json('Not Acceptable');
+	} else {
+		items[index] = { ...items[index], count: items[index].count + req.body.deposit };
+		return res.status(200).json('Updated');
+	}
 });
 
 //Connect to Server
